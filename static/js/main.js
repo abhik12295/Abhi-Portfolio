50df3f998b11533dd1db228cc77de0ec6a1f79cd
Abhishek Kumar <abhik12295@gmail.com>
2021-11-15 14:50:45 -0600

    made changes

--- static/js/main.js
+++ static/js/main.js
@@ -99,3 +99,19 @@ window.addEventListener('keydown', function (event) {
         })
     }
   })
+
+/*==================== Portfolio Swipper ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
